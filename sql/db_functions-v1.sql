634c24999629a736b0b5ed16bb0bf81ca79d2fc3
Sergey Fedorov <31002143+Punctuality@users.noreply.github.com>
2020-12-18 12:53:05 +0300

    Updated functions #2

--- sql/db_functions-v1.sql
+++ sql/db_functions-v1.sql
@@ -15,62 +15,43 @@ create or replace function create_worker(
     end;
 $$ language plpgsql;
 
-create or replace function company_has_film(company uuid) returns boolean as
+-------------
+
+create or replace function company_has_films(c_id uuid) returns boolean as
 $$
 declare
     checker integer;
 begin
-    checker = (select count()
-               from company c
-                        join film f on f.company_id = c.id);
+    checker = (
+        select count()
+        from company c
+        join film f on f.company_id = c.id
+        where c.id = c_id
+    );
+
     if (checker > 0) then
         return true;
     else
         return false;
     end if;
 end;
-$$
-    language plpgsql;
+$$ language plpgsql;
 
 -------------
 
-create or replace function amount_of_films(company uuid) returns integer as
+create or replace function amount_of_films(c_id uuid) returns integer as
 $$
 declare
     amount integer;
 begin
-    amount = (select count()
-              from company c
-                       join film f on f.company_id = c.id);
+    amount = (
+        select count()
+        from company c
+        join film f on f.company_id = c.id
+        where c.id = c_id
+    );
     return amount;
 end;
-$$
-    language plpgsql;
+$$ language plpgsql;
 
 ------------------
-
-create or replace function amount_of_films(company uuid) returns integer as
-$$
-declare
-    amount integer;
-begin
-    amount = (select count()
-              from company c
-                       join film f on f.company_id = c.id);
-    return amount;
-end;
-$$
-    language plpgsql;
-
-
-------------------------
-
-
--- uuid_generate_v4()
-create or replace function create_employee(id_e uuid, age_e integer, description text, name_e varchar(255), gender varchar(255), surname_e varchar(255)) returns void as
-$$
-begin
-    INSERT INTO employee VALUES (id_e, age_e, description, name_e, gender, surname_e);
-end;
-$$
-    language plpgsql;
