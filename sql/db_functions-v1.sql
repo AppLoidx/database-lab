4d406784c7ea7e7c757d83797a54e8a096d33793
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-12-18 18:47:55 +0900

    Update db_functions-v1.sql

--- sql/db_functions-v1.sql
+++ sql/db_functions-v1.sql
@@ -14,3 +14,63 @@ create or replace function create_worker(
         return 'Worker created. Id: ' || id_employee;
     end;
 $$ language plpgsql;
+
+create or replace function company_has_film(company uuid) returns boolean as
+$$
+declare
+    checker integer;
+begin
+    checker = (select count()
+               from company c
+                        join film f on f.company_id = c.id);
+    if (checker > 0) then
+        return true;
+    else
+        return false;
+    end if;
+end;
+$$
+    language plpgsql;
+
+-------------
+
+create or replace function amount_of_films(company uuid) returns integer as
+$$
+declare
+    amount integer;
+begin
+    amount = (select count()
+              from company c
+                       join film f on f.company_id = c.id);
+    return amount;
+end;
+$$
+    language plpgsql;
+
+------------------
+
+create or replace function amount_of_films(company uuid) returns integer as
+$$
+declare
+    amount integer;
+begin
+    amount = (select count()
+              from company c
+                       join film f on f.company_id = c.id);
+    return amount;
+end;
+$$
+    language plpgsql;
+
+
+------------------------
+
+
+-- uuid_generate_v4()
+create or replace function create_employee(id_e uuid, age_e integer, description text, name_e varchar(255), gender varchar(255), surname_e varchar(255)) returns void as
+$$
+begin
+    INSERT INTO employee VALUES (id_e, age_e, description, name_e, gender, surname_e);
+end;
+$$
+    language plpgsql;
