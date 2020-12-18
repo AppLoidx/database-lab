c956e2627901470806509f5d17ee146066daaa02
Sergey Fedorov <31002143+Punctuality@users.noreply.github.com>
2020-12-18 13:19:17 +0300

    Updated functions #3

--- sql/db_functions-v1.sql
+++ sql/db_functions-v1.sql
@@ -15,6 +15,48 @@ create or replace function create_worker(
     end;
 $$ language plpgsql;
 
+create or replace function create_company(
+    new_name varchar, new_business_type varchar, new_manager_name varchar,
+    new_bik bigint, new_inn bigint, new_ogrn bigint
+) returns text as $$
+    declare
+        id_company uuid;
+    begin
+        insert into company(bik, business_type, inn, manager_legal_name, name, ogrn)
+        values (new_bik, new_business_type, new_inn, new_manager_name, new_name, new_ogrn)
+        returning company.id into id_company;
+
+        return 'Company created. Id: ' || id_company;
+    end;
+$$ language plpgsql;
+
+-------------
+
+create or replace function create_contract(
+    company_id uuid, employee_id uuid, -- REFERENCES
+    created_date timestamp, end_date timestamp,
+    name varchar, description varchar, doc_url varchar
+) returns text as $$
+    declare
+        id_contract uuid;
+    begin
+        if exists(select c.id from company c where c.id = company_id) then
+            if exists(select e.id from employee e where e.id = employee_id) then
+                insert into contract(created_date, description, doc_url, end_date, interrupted, interrupted_date, name, printed, company_id, employee_id)
+                values (created_date, description, doc_url, end_date, false, null, name, false, company_id, employee_id)
+                returning contract.id into id_contract;
+
+                return 'Contract created. Id: ' || id_contract;
+            else
+                return 'No such employee_id: ' || employee_id;
+            end if;
+        else
+            return 'No such company_id: ' || company_id;
+
+        end if;
+    end;
+$$ language plpgsql;
+
 -------------
 
 create or replace function company_has_films(c_id uuid) returns boolean as
